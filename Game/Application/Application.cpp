5600772919b1f86603843a02df2d42387c4a300a
egyhurka <levente.mihaly9@gmail.com>
2024-12-23 22:33:29 +0100

    Add Test

--- Game/Application/Application.cpp
+++ Game/Application/Application.cpp
@@ -1,10 +1,15 @@
 #include <Core\Engine.h>
 
+#include "test.h"
+
 #include <iostream>
 
 int main()
 {
 	Engine engine(800, 600, "Engine");
+
+	//Test::error("An error occurred: ", 404, " Not Found");
+
 
 	engine.run();
 
