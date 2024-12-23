e34ef795064e41a87e71230d7070854a8a568bc6
egyhurka <levente.mihaly9@gmail.com>
2024-12-23 20:53:02 +0100

    Add comment to Log

--- Engine/src/Core/Log.h
+++ Engine/src/Core/Log.h
@@ -8,5 +8,5 @@ namespace Log
 	void debug(std::string message);	// DEBUG PRINT
 	void error(std::string error);		// ERROR PRINT
 	void key(int key, int action);		// KEY PRINT
-	void mouse(int button, int action);
+	void mouse(int button, int action); // MOUSE PRINT
 }
